a9001a4e8a1fe468e4f196de3aff789f7f0ab142
Chance03-git <Chancefoster05@gmail.com>
2024-11-28 22:33:29 -0500

    Implement function to insert data into a binary tree node and traverse it in-order

--- nodes2.java
+++ nodes2.java
@@ -1,6 +1,48 @@
 import org.w3c.dom.Node;
 
 public class nodes2 {
+    static class Node {
+        private int data;      // Integer data stored in the node
+        private Node left;     // Left child node
+        private Node right;    // Right child node
+
+        // Constructor
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+        public void insert(int newData) {
+            if (newData < this.data) {
+                // Insert into the left child
+                if (this.left == null) {
+                    this.left = new Node(newData);
+                } else {
+                    this.left.insert(newData);
+                }
+            } else {
+                // Insert into the right child
+                if (this.right == null) {
+                    this.right = new Node(newData);
+                } else {
+                    this.right.insert(newData);
+                }
+            }
+        }
+
+        /**
+         * Traverses the tree in-order (left, root, right) and prints the data.
+         */
+        public void printInOrder() {
+            if (left != null) {
+                left.printInOrder();
+            }
+            System.out.print(data + " ");
+            if (right != null) {
+                right.printInOrder();
+            }
+        }
+    }
     // Main method to test the implementation
     public static void main(String[] args) {
         // Create the root node
